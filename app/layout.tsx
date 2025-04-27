e616d29609b3511460952f289c7e61544f582e62
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-27 18:08:17 +0530

    integrate Clerk for authentication and add Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import Header from "@/components/Header";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -15,11 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${inter.className} antialiased`}
       >
-
+        <Header />
         <main className="min-h-screen ">
           {children}
         </main>
@@ -30,5 +35,6 @@ export default function RootLayout({
         </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav className='flex items-center justify-between'>
+
+        </nav>
+    )
+}
+
+export default Header
