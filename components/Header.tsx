38fd7b174d23b650c6e402494a8b06d3d1989c21
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-27 20:37:06 +0530

    feat: update Header component to support admin navigation and include login link

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,13 @@ import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@cl
 import Link from 'next/link'
 import React from 'react'
 
-const Header = () => {
+const Header = ({ isAdminpage = false }) => {
+    const isAdmin = false
     return (
         <header className='px-8 py-4 bg-gray-800 fixed top-0 left-0  max-w-full w-full z-10'>
 
             <nav className='text-white flex justify-between'>
-                <Link href="/" className='text-2xl text-white font-medium'>
+                <Link href={isAdminpage ? "/admin" : "/"} className='text-2xl text-white font-medium'>
                     <span className='mr-2 font-light text-red-400'>  Auto</span>Quest
                 </Link>
                 <div className='flex gap-4'>
@@ -15,6 +16,7 @@ const Header = () => {
                     <Link href={"/"}>Services</Link>
                     <Link href={"/"}>Cars</Link>
                     <Link href={"/"}>Dealers</Link>
+                    <Link href={"/signin"}>Login</Link>
                 </div>
 
             </nav>
