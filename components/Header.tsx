f679c27e5b459e929a9b115e4a34aba6d3aa0fb5
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-27 18:09:38 +0530

    feat: update Header component to include authentication buttons for signed in and signed out states

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Header = () => {
     return (
         <nav className='flex items-center justify-between'>
-
+            <SignedOut>
+                <SignInButton />
+                <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
         </nav>
     )
 }
